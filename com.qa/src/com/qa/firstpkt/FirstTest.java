33b8a0cb95f3e15634559e95e84380914a7e4e71
vinodcse99 <vinod.gogula6@gmail.com>
2020-03-10 21:38:30 +0530

    latest file

---
+++ com.qa/src/com/qa/firstpkt/FirstTest.java
@@ -0,0 +1,13 @@
+package com.qa.firstpkt;
+
+public class FirstTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		System.out.println("again one more project");
+
+	}
+
+}
